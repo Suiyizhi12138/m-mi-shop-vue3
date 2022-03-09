80eae6bab5771496a8b2e406b2c0f8b5bca39c58
Robo12138 <1607360407@qq.com>
2022-03-09 17:37:39 +0800

    'route'

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://m.efront.top/index/api'
+const API_URL = 'http://m.efront.top/index.php/api'
 const DOMAIN = 'http://m.efront.top'
 const AMAP_URL = '/v3'
 const AMAP_KEY_WEB = '56f660b153d242ab5dd813d5498b1074'
