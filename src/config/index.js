dbda8a5a294b8442b96eab13ef3eb5c4f057dc6b
Eilan Chiu <37210877+Suiyizhi12138@users.noreply.github.com>
2022-09-17 15:02:57 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const API_URL = 'http://m.efront.top/index.php/api'
 const DOMAIN = 'http://m.eftont.top'
 const AMAP_URL = '/v3'
-const AMAP_KEY_WEB = '56f660b153d242ab5dd813d5498b1074'
+const AMAP_KEY_WEB = '56f660b153d*****13d5498b1074'
 export default{
   API_URL,
   DOMAIN,
